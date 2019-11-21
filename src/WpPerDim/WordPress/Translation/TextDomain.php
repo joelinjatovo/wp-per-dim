e8a682120dde0f04aed0e74d4b4f8f62b5e3a7a7
Joelinjatovo <joelinjatovo@gmail.com>
2019-11-21 13:39:45 +0300

    Last modif

--- src/WpPerDim/WordPress/Translation/TextDomain.php
+++ src/WpPerDim/WordPress/Translation/TextDomain.php
@@ -18,8 +18,6 @@ class TextDomain implements HooksInterface{
      * @see WpPerDim\Interfaces\HooksInterface
      */
     public function hooks(){
-        //add_action( 'init', array( $this, 'load_textdomain' ) );
-        //add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
         $this->load_textdomain();
     }
 			
@@ -27,8 +25,8 @@ class TextDomain implements HooksInterface{
      * Load plugin textdomain.
      */
     public function load_textdomain() {
-        $plugin_rel_path = 'WpPerDim/languages/';
-        load_plugin_textdomain('WpPerDim', false, $plugin_rel_path );
+        $plugin_rel_path = 'wp-per-dim/languages/';
+        load_plugin_textdomain('wppd', false, $plugin_rel_path );
     }
     
 }
